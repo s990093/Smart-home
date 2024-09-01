a825bf36c24b3ce0dc2b80ae2ac99ce3fbd503ad
Your Name <you@example.com>
2024-09-01 19:33:00 +0800

    init swiftui code

---
+++ Smart-home/Smart-home/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Smart-home
+//
+//  Created by hungwei on 2024/9/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Smart-home/Smart-home/Smart_homeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_homeApp.swift
+//  Smart-home
+//
+//  Created by hungwei on 2024/9/1.
+//
+
+import SwiftUI
+
+@main
+struct Smart_homeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
